01125c58f7e1b9eab66741ff862c3fb4d6d086d3
3omd4 <mohamedemadeldin38@gmail.com>
2024-11-25 02:04:56 +0000

    setupt.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/car_control/action', ['action/WallFollowing.action']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
