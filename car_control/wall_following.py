860edac8603be48e816bebbf96b23f4f070543c2
3omd4 <mohamedemadeldin38@gmail.com>
2024-11-28 22:46:17 +0000

    wall_following=10.6

--- car_control/wall_following.py
+++ car_control/wall_following.py
@@ -84,7 +84,6 @@ def main(arg = None):
     node=rclpy.create_node('PID_wall_following')
     steering_pub= node.create_publisher(Float32, 'autodrive/f1tenth_1/steering_command', 0)
     throttle_pub= node.create_publisher(Float32, 'autodrive/f1tenth_1/throttle_command', 0)
-    labs_count_sub = node.create_subscription(Int32, '/autodrive/f1tenth_1/lap_count', lap_count_callback, 0)
     lidar_sub=node.create_subscription(LaserScan, '/autodrive/f1tenth_1/lidar', lidar_callback, 0)
 
     try:
