8c0ecfeeaf9df825b1057f11ce54760a73bf3b0a
3omd4 <mohamedemadeldin38@gmail.com>
2024-11-28 22:56:53 +0000

    wall_following=10.6

--- car_control/wall_following.py
+++ car_control/wall_following.py
@@ -64,11 +64,11 @@ def lidar_callback(scan):
     prev_rd=D_R
     prev_steering=steering_angle.data
 
-    throttle.data=0.11
+    throttle.data=0.12
     if min(scan.ranges)>=0.4 and abs(angle_to_Rwall)<=0.05 and abs(angle_to_Lwall)<=0.02:
-        throttle.data=0.25#1.3
+        throttle.data=0.7#1.3
     if min(scan.ranges)>=0.6 and abs(angle_to_Rwall)<=0.02 and abs(angle_to_Lwall)<=0.02:
-        throttle.data=0.4
+        throttle.data=0.7
     if(lap_count>=12):
         throttle.data=0.00
     throttle_pub.publish(throttle)
