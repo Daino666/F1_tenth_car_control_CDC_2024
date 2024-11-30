c91b19133a2c728347ffaee28cd0304588aae558
Daino666 <Omarabbas3210@gmail.com>
2024-11-30 19:05:05 +0000

    uploading yaml data and and updating map

--- car_control/SlamToOccupency.py
+++ car_control/SlamToOccupency.py
@@ -1,8 +1,12 @@
+import rclpy
+from nav_msgs.msg import OccupancyGrid, MapMetaData
+from geometry_msgs.msg import Pose
 import numpy as np 
 import matplotlib.pyplot as plt 
 import networkx as nx
 import yaml
 from PIL import Image 
+
 
 
 def load_yaml(yaml_path):
@@ -20,6 +24,51 @@ def pgm_to_occupancy(pgm_path):
 
     return occupancy_grid
 
+
+def create_occupancy_grid(occupany_grid, yaml_data):
+    race_map = MapMetaData()
+    race_map.resolution = yaml_data['resolution']
+    race_map.width = occupany_grid.shape[1]
+    race_map.height = occupany_grid.shape[0]
+    race_map.origin = Pose()
+    race_map.origin.position.x = yaml_data['origin'][0]
+    race_map.origin.position.y = yaml_data['origin'][1]
+    race_map.origin.position.z = 0.0
+    race_map.origin.orientation.w = float(yaml_data['origin'][2])
+    occupied_thresh = yaml_data['occupied_thresh']
+    free_thresh = yaml_data['free_thresh']
+
+
+    for y in range(race_map.height):
+
+        for x in range(race_map.width):
+
+            if occupany_grid[y][x] >= occupied_thresh:
+                occupany_grid[y][x] = 1
+
+            elif occupany_grid[y][x] <= free_thresh:
+                occupany_grid[y][x] = 0
+
+            else:
+                occupany_grid[y][x] = -1
+
+    
+    grid_data = occupany_grid.flatten().tolist()
+
+    occ_grid_msg = OccupancyGrid()
+    occ_grid_msg.header.frame_id = "map"
+    occ_grid_msg.info = race_map
+    occ_grid_msg.data = grid_data
+
+    return  occ_grid_msg ,occupany_grid
+
+
+
+
+
+
+
+
 def visualization(occ_grid):
     plt.imshow(occ_grid, cmap='YlGnBu')
     plt.colorbar(label='Occupancy Probability')
@@ -31,7 +80,9 @@ def visualization(occ_grid):
 
 def main():
 
+    yaml_data = load_yaml('/home/autodrive_devkit/src/car_control/car_control/maps/iros_2024/iros_map_compete2024.yaml')
     occupancy_grid = pgm_to_occupancy('/home/autodrive_devkit/src/car_control/car_control/maps/iros_2024/iros_map_compete2024.pgm')
+    grid_msg , occupancy_grid = create_occupancy_grid(occupancy_grid,yaml_data)
 
     visualization(occupancy_grid)
 
